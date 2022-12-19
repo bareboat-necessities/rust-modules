4e4fb49ae68d983f77b0a75d9c322cfcf2bdda43
mgrouch <mgrushinskiy@gmail.com>
2022-12-19 11:11:06 -0500

    added

---
+++ gpsd-client-rust/src/main.rs
@@ -0,0 +1,44 @@
+#[macro_use]
+extern crate log;
+
+use futures::prelude::*;
+use gpsd_proto::UnifiedResponse;
+use futures::StreamExt;
+use tokio_util::codec::Framed;
+use tokio_util::codec::LinesCodec;
+use tokio::net::TcpStream;
+use std::error::Error;
+use std::net::{SocketAddr};
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    let saddr: SocketAddr = "127.0.0.1:2947".parse().unwrap();
+    let conn = TcpStream::connect(saddr).await?;
+
+    let mut framed = Framed::new(conn, LinesCodec::new_with_max_length(64 * 1024));
+    framed.send(gpsd_proto::ENABLE_WATCH_CMD.to_string()).await?;
+    while let Some(Ok(line)) = framed.next().await {
+        match serde_json::from_str(line.as_str()) {
+            Ok(rd) => match rd {
+                UnifiedResponse::Version(v) => {
+                    if v.proto_major < gpsd_proto::PROTO_MAJOR_MIN {
+                        panic!("Gpsd major version mismatch");
+                    }
+                    info!("Gpsd version {} connected", v.rev);
+                }
+                UnifiedResponse::Devices(_) => {}
+                UnifiedResponse::Watch(_) => {}
+                UnifiedResponse::Device(d) => debug!("Device {:?}", d),
+                UnifiedResponse::Tpv(t) => debug!("Tpv {:?}", t),
+                UnifiedResponse::Sky(s) => debug!("Sky {:?}", s),
+                UnifiedResponse::Pps(p) => debug!("PPS {:?}", p),
+                UnifiedResponse::Gst(g) => debug!("GST {:?}", g),
+            },
+            Err(e) => {
+                error!("Error decoding: {}", e);
+            }
+        };
+    }
+
+    Ok({})
+}
