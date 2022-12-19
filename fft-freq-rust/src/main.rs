0ac2d9007056ef7222b620fd12c8a1f4b64dbdb4
mgrouch <mgrushinskiy@gmail.com>
2022-12-18 19:02:40 -0500

    added

--- fft-freq-rust/src/main.rs
+++ fft-freq-rust/src/main.rs
@@ -7,7 +7,7 @@ const PI: f64 = std::f64::consts::PI;
 
 fn main() {
     // Set the sample rate and number of samples
-    let sample_rate =   100;  // Hz
+    let sample_rate =  100;  // Hz
     let num_samples = 1024;
 
     // Generate some test data
@@ -17,14 +17,18 @@ fn main() {
         let value = (2.0 * PI * t).sin() + 5.0 * (2.0 * PI * 10.0 * t).sin();
         data.push(Complex::new(value, 0.0));
     }
+    let main_freq = main_freq(sample_rate, &mut data);
+    println!("Main frequency: {} Hz", main_freq);
+}
 
+fn main_freq(sample_rate_hz: i32, mut data: &mut Vec<Complex<f64>>) -> f64 {
     // Create an FFT planner
     let mut planner = FftPlanner::new();
 
     // Plan an FFT of the data
-    let fft = planner.plan_fft_forward(num_samples);
+    let fft = planner.plan_fft_forward(data.len());
 
-    // Perform the FFT
+    // Perform the FFT in-place
     fft.process(&mut data);
 
     // Find the main frequency
@@ -39,7 +43,6 @@ fn main() {
     }
 
     // Convert the main frequency to Hz
-    let main_freq = main_freq as f64 * sample_rate as f64 / num_samples as f64;
-
-    println!("Main frequency: {} Hz", main_freq);
+    let main_freq = main_freq as f64 * sample_rate_hz as f64 / data.len() as f64;
+    main_freq
 }
