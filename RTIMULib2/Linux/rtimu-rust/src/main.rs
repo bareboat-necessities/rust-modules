6b8d8d495e0c15548b3eb20f8f15309fb7318cb0
Mikhail Grushinskiy <mgrouch@users.noreply.github.com>
2023-03-10 14:51:43 +0000

    Update main.rs

--- RTIMULib2/Linux/rtimu-rust/src/main.rs
+++ RTIMULib2/Linux/rtimu-rust/src/main.rs
@@ -1,7 +1,7 @@
 use std::ffi::{CStr};
 use std::mem::MaybeUninit;
 use std::{ptr, thread, time};
-use rtimulib_rust::{RTIMUSettings, RTIMU, RTIMUSettings_RTIMUSettings, RTIMUMPU925x_IMUInit, RTIMUMPU925x_IMUGetPollInterval, RTMath_displayDegrees, RTVector3, RTIMUMPU925x_IMURead, RTIMUMPU925x_RTIMUMPU925x_destructor, RTFusionKalman4_RTFusionKalman4_destructor};
+use rtimulib_rust::{RTIMUSettings, RTIMU, RTIMUSettings_RTIMUSettings, RTIMUMPU925x_IMUInit, RTIMUMPU925x_IMUGetPollInterval, RTMath_displayDegrees, RTIMUMPU925x_IMURead, RTIMUMPU925x_RTIMUMPU925x_destructor, RTFusionKalman4_RTFusionKalman4_destructor};
 
 fn main() {
     println!("Hello RTIMULib!");
@@ -9,7 +9,7 @@ fn main() {
     let mut instance = MaybeUninit::<RTIMUSettings>::uninit();
     let settings = instance.as_mut_ptr();
     unsafe {
-        RTIMUSettings_RTIMUSettings( settings, "RTIMULib".as_ptr());
+        RTIMUSettings_RTIMUSettings(settings, "RTIMULib".as_ptr());
         (*settings).m_fusionType = 1;
         let imu = RTIMU::createIMU(settings);
         RTIMUMPU925x_IMUInit(imu as *mut _);
@@ -27,12 +27,12 @@ fn main() {
 
         RTIMUMPU925x_IMURead(imu as *mut _);
         let mut imu_data = (*imu).m_imuData;
-        let p_pose : *mut RTVector3 = ptr::addr_of_mut!(imu_data.fusionPose);
+        let p_pose = ptr::addr_of_mut!(imu_data.fusionPose);
 
         let deg = RTMath_displayDegrees("".as_ptr(), p_pose);
         println!("{}", CStr::from_ptr(deg).to_str().unwrap().to_owned());
 
         RTFusionKalman4_RTFusionKalman4_destructor(fusion as *mut _);
-        RTIMUMPU925x_RTIMUMPU925x_destructor( imu as *mut _);
+        RTIMUMPU925x_RTIMUMPU925x_destructor(imu as *mut _);
     }
 }
