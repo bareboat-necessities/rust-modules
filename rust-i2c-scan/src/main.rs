4d45e2af31ae2b0709e9677593020d6c63f388a7
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-06-04 09:04:09 -0400

    fixes

--- rust-i2c-scan/src/main.rs
+++ rust-i2c-scan/src/main.rs
@@ -1,25 +1,34 @@
 extern crate i2cdev;
 
-// NOTE: It doesn't work
-
 #[cfg(any(target_os = "linux", target_os = "android"))]
 use i2cdev::linux::*;
 
 fn main() {
 
-    use crate::i2cdev::core::I2CTransfer;
+    use crate::i2cdev::core::I2CDevice;
 
-    for addr in 1..=127 {
-        println!("Scanning Address {:#02x}", addr as u8);
+    for addr in 0..=127 {
+        //println!("Scanning Address {:#02x}", addr as u8);
 
         // Scan Address
-        let mut dev = LinuxI2CDevice::new("/dev/i2c-1", addr);
-        let mut read_data = [0; 2];
-        let mut msgs = [
-            LinuxI2CMessage::write(&[0x01]),
-            LinuxI2CMessage::read(&mut read_data).with_address(addr as u16)
-        ];
-        dev.expect("Reason").transfer(&mut msgs);
-        println!("Read: {:?}", read_data);
+        let mut dev = LinuxI2CDevice::new("/dev/i2c-1", addr).unwrap();
+        if (addr >= 0x30 && addr <= 0x37) || (addr >= 0x50 && addr <= 0x57) {
+            match dev.smbus_read_byte() {
+                Ok(_val) => {
+                    println!("Found Address {:#02x}", addr as u8);
+                },
+                Err(_err) => {
+                },
+            }
+        }
+        else {
+            match dev.smbus_write_quick(false) {
+                Ok(_val) => {
+                    println!("Found Address {:#02x}", addr as u8);
+                },
+                Err(_err) => {
+                },
+            }
+        }
     }
 }
