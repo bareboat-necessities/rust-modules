1a5e19bff17e29065d265782a726c461ec7c0643
Mikhail Grushinskiy <mgrushinskiy@gmail.com>
2024-06-04 09:53:33 -0400

    fixes

--- rust-i2c-scan/src/main.rs
+++ rust-i2c-scan/src/main.rs
@@ -3,6 +3,35 @@ extern crate i2cdev;
 #[cfg(any(target_os = "linux", target_os = "android"))]
 use i2cdev::linux::*;
 
+type I2cDeviceInfo = (&'static str, &'static str, &'static [u8]);
+
+const I2C_SCANNER_KNOWN_DEVICES: [I2cDeviceInfo; 13] = [
+    ("MPU6050", "Six-Axis (Gyro + Accelerometer) MEMS MotionTracking Devices", &[0x68, 0x69]),
+    ("MPU-9250", "3-Axis Gyroscope and Accelerometer", &[0x68]),
+    ("MPU-9250", "9-DoF IMU Gyroscope, Accelerometer and Magnetometer", &[0x68, 0x69]),
+    ("PCF8523", "RTC", &[0x68]),
+    ("BMA180", "Accelerometer", &[0x77]),
+    ("BME280", "Temp/Barometric/Humidity", &[0x77, 0x76]),
+    ("BME680", "Low power gas, pressure, temperature & humidity sensor", &[0x77, 0x76]),
+    ("BME688", "Digital low power gas, pressure, temperature and humidity sensor with AI", &[0x77, 0x76]),
+    ("BMP085", "Temp/Barometric", &[0x77]),
+    ("BMP180", "Temp/Barometric", &[0x77]),
+    ("BMP280", "Temp/Barometric", &[0x77, 0x76]),
+    ("MS5607", "Barometric Pressure", &[0x77, 0x76]),
+    ("MS5611", "Barometric Pressure", &[0x77, 0x76]),
+];
+
+fn lookup(addr: u8) {
+    for i in 0 .. I2C_SCANNER_KNOWN_DEVICES.len() {
+        let addresses = I2C_SCANNER_KNOWN_DEVICES[i].2;
+        for j in 0 .. addresses.len() {
+            if addr == addresses[j] {
+                println!("  {}  {}", I2C_SCANNER_KNOWN_DEVICES[i].0, I2C_SCANNER_KNOWN_DEVICES[i].1);
+            }
+        }
+    }
+}
+
 fn main() {
 
     use crate::i2cdev::core::I2CDevice;
@@ -16,6 +45,7 @@ fn main() {
             match dev.smbus_read_byte() {
                 Ok(_val) => {
                     println!("Found Address {:#02x}", addr as u8);
+                    lookup(addr as u8);
                 },
                 Err(_err) => {
                 },
@@ -25,6 +55,7 @@ fn main() {
             match dev.smbus_write_quick(false) {
                 Ok(_val) => {
                     println!("Found Address {:#02x}", addr as u8);
+                    lookup(addr as u8);
                 },
                 Err(_err) => {
                 },
